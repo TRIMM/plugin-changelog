e3e0cf06e696c87196ec9f7a8521ed7845d4e754
Maarten Heebink <maarten.heebink@trimm.nl>
2022-09-09 14:33:32 +0200

    fix: forget React include

--- src/plugin.ts
+++ src/plugin.ts
@@ -7,6 +7,7 @@ import {
   discoveryApiRef,
 } from '@backstage/core-plugin-api';
 import { ChangelogApiRef, ChangelogStubClient } from './api';
+import React from 'react';
 
 export const rootRouteRef = createRouteRef({
   id: 'Changelog',
@@ -16,7 +17,7 @@ export const changelogPlugin = createPlugin({
   id: 'Changelog',
   apis: [
     createApiFactory({
-      api: ChangelogApiRef,
+      api: ChangelogApiRef, 
       deps: { configApi: configApiRef, discoveryApi: discoveryApiRef },
       factory: ({ discoveryApi }) => 
         new ChangelogStubClient({
